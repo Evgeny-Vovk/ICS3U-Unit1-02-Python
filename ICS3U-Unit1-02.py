2b151da633245e0c734352fa0b0bb810c410ab18
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-15 15:21:23 +0000

    fixed file

--- ICS3U-Unit1-02.py
+++ ICS3U-Unit1-02.py
@@ -2,7 +2,7 @@
 #
 # Created by: Evgeny Vovk
 # Created on: September 2022
-# ICS3U-Unit1-02.py File, learning how to commit changes
+# ICS3U-Unit1-02.py File, learning how to commit changes.
 
 print("ICS3U-Unit1-02 lesson,")
 print("Made by Evgeny.")
